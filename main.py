2e640cbea151094dfb94f97bfecb17f1ae1e4459
imsiriuse <imsiriuse@gmail.com>
2022-07-22 15:24:18 +0200

    tests from main.py was removed

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
-# import webscraper
-# import config as CFG
-from firefoxmachine import FirefoxMachine
+import webscraper
+import config as CFG
 
 if __name__ == '__main__':
 
-    # config = CFG.Config("https://localhost:4321/test1/")
-    # scraper = webscraper.Scraper(config)
-    # scraper.start()
-    #
+    config = CFG.Config("https://localhost:4321/test1/")
+    scraper = webscraper.Scraper(config)
+    scraper.start()
 
-    #
-    # HONEYPOT TEST
-    #
-    machine = FirefoxMachine(windowsize="1024,768", honeypots=True)
-    try:
-        machine.loadurl("http://localhost:4321/test1/testing-page/")
-        print(machine.driver.current_url)
-        machine.clicklink("http://localhost:4321/test1/product-category/originals/", "p#displaynone > a")
-        print(machine.driver.current_url)
-        machine.clicklink("http://localhost:4321/test1/product-category/core-neo/", "p#opacity0 > a")
-        print(machine.driver.current_url)
-    finally:
-        machine.driver.quit()
